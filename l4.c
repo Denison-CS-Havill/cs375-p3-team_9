e6677c41c87d1f58598c5e4986439fb36ea4a88f
XDuoFan <42943270+XDuoFan@users.noreply.github.com>
2022-03-07 18:56:00 -0500

    l4

--- l4.c
+++ l4.c
@@ -7,10 +7,23 @@ extern int l3_write(char* buffer, int length);
 
 int l4_read(char* name, int* nameLength, char* value, int* valueLength)
 {
- 
+    char buff[1000];
+    int length = l3_read(buff, 1000);
+    int i = 1;
+    while ( (buff[i]!=',') )
 }
 
 int l4_write(char* name, int nameLength, char* value, int valueLength)
 {
- 
+    char buff[nameLength+valueLength+3];
+    strcpy(buff,'(');
+    strcat(buff, name);
+    strcat(buff, ',');
+    strcat(buff, value);
+    strcat(buff,')');
+    if (l3_write(buff, nameLength+valueLength+3) ==-1){
+        return 0;
+    }
+
+    return 1;
 }
