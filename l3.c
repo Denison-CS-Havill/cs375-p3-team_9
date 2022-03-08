03abd88615f1ecd578a6683483a71dd10af241ea
brnngn <60111880+brnngn@users.noreply.github.com>
2022-03-07 20:38:52 -0500

    Update l3.c

--- l3.c
+++ l3.c
@@ -4,59 +4,49 @@
 extern int l2_read(char* buffer, int maxlength);
 extern int l2_write(char* buffer, int length);
 
-u_long checksum(char* buffer, int length){
-    register u_long sum = 0;
-
-     while(length--)
-    {
-        sum += *buffer;
-        buffer++;
-    };
-    return ~(sum & 0xFF);
+unsigned char checksum(char* buffer, int length){
+    unsigned char sum;
+    sum = *buffer;
+    for (int i = 1; i<length; i++){
+        sum = (char) (sum + *(buffer+i));
+    }
+    return sum;
 }
-
 
 int l3_read(char* buffer, int maxlength)
 {
-    char header[4];
-    short convert;
-    u_long checksum = 0;
-    for(int i = 0; i<4; i++){
-        if (l2_read(&header[i]) == -1){
-            return -1;
-        }
-        convert = (short) header[i];
-        checksum += convert << (8*4*(3-i));
-    }
-    checksum = ntohl(checksum);
     
+    int messageLength = l2_read(buffer, maxlength);
+    if ( (messageLength == -1)|(messageLength > maxlength) ){
+        return -1;
+    } 
 
+    char checksumOfMessage = checksum(buffer, messageLength) &= 0x7F ;
+
+    if (checksumOfMessage == 0){
+        return messageLength;
+    } else {
+        return -1;
+    }
 }
 
 
 int l3_write(char* buffer, int length)
 {
-    u_long checksumOutput = htonl(checksum(buffer, length));
+    unsigned char checksumOutput = checksum(buffer, length);
+    checksumOutput = (char) ((-checksumOutput) & 0x7F);
 
-    char header[4];
-    u_short converting;
-    u_long convert_l;
-    for (int i = 0; i<length; i++){
-        convert_l = fmod((checksumOutPut >> (8*4*i)) , pow(2,8*4) );
-        converting = (short) convert_l;
-        header[3-i] = (char) converting;
-    }
-    
+    char header[1];
+    header[0] = checksumOutput;
+
     char* newBuffer;
 
     newBuffer = malloc(strlen(header)+1+length);
     strcpy(newBuffer, header);
     strcat(newBuffer, buffer);
 
-    for (int i=0; i<strlen(newBuffer); i++){
-        if(l2_write(newBuffer[i]) == -1){
-            return -1;
-        }
+    if(l2_write(newBuffer, length+1) == -1){
+        return -1;
     }
 
     return length;
