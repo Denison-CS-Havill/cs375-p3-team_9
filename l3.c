2d8e9f982e6a4734cb3a1a51f062d82a247be2c3
brnngn <60111880+brnngn@users.noreply.github.com>
2022-03-08 20:26:46 -0500

    Update l3.c

--- l3.c
+++ l3.c
@@ -1,56 +1,103 @@
 // l3.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 // Interface to Layer 2
 extern int l2_read(char* buffer, int maxlength);
 extern int l2_write(char* buffer, int length);
 
-unsigned char checksum(char* buffer, int length){
-    unsigned char sum;
-    sum = *buffer;
-    for (int i = 1; i<length; i++){
-        sum = (char) (sum + *(buffer+i));
+// unsigned char checksum(char* buffer, int length){
+//     unsigned char sum;
+//     sum = *buffer;
+//     for (int i = 1; i<length; i++){
+//         sum = (char) (sum + *(buffer+i));
+//     }
+//     return sum;
+// }
+
+unsigned short checksum(char* buffer, int count){
+    register unsigned long sum = 0;
+
+    while(count--){
+        sum += *buffer++;
+        if (sum & 0xFFFF0000){
+            sum &= 0xFFFF;
+            sum++;
+        }
     }
-    return sum;
+    return ~(sum & 0xFFFF);
 }
 
-int l3_read(char* buffer, int maxlength)
-{
-    char messageWithHeader[maxlength];
+
+// int l3_read(char* buffer, int maxlength)
+// {
+//     char *messageWithHeader;
+//     int messageLength = l2_read(messageWithHeader, maxlength);
+//     if ( (messageLength == -1)|(messageLength > maxlength) ){
+//         return -1;
+//     } 
+
+//     char checksumOfMessage = checksum(messageWithHeader, messageLength) &= 0x7F ;
+
+//     if (checksumOfMessage == 0){
+//         for (int i = 1; i < messageLength; i++){
+//             *(buffer+i-1) = messageWithHeader[i];
+//         }
+//         free(messageWithHeader);
+//         return messageLength;
+//     } else {
+//         return -1;
+//     }
+// }
+
+int l3_read(char* buffer, int maxlength){
+    char *messageWithHeader;
     int messageLength = l2_read(messageWithHeader, maxlength);
     if ( (messageLength == -1)|(messageLength > maxlength) ){
         return -1;
-    } 
+    }
 
-    char cheksum = checksum(messageWithHeader, messageLength);
-    char checksumOfMessage = (cheksum & 0xFF) ;
-
-    if (checksumOfMessage == 0){
+    unsigned short checksum = checksum(messageWithHeader, messageLength);
+    if (checksum == 0){
         for (int i = 1; i < messageLength; i++){
             *(buffer+i-1) = messageWithHeader[i];
         }
-        //free(messageWithHeader);
         return messageLength;
     } else {
-        //printf("checksum: ");
-        //printf("%s\n",messageWithHeader);
         return -1;
     }
 }
 
+// int l3_write(char* buffer, int length)
+// {
+    
+//     unsigned char checksumOutput = checksum(buffer, length);
+//     checksumOutput = (char) ((-checksumOutput) & 0x7F);
 
-int l3_write(char* buffer, int length)
-{
-    unsigned char checksumOutput = checksum(buffer, length);
-    checksumOutput = (char) ((-checksumOutput) & 0xFF);
+//     char header[1];
+//     header[0] = checksumOutput;
 
-    char header;
-    header = checksumOutput;
+//     char* newBuffer;
 
-    char newBuffer[1+length];
-    newBuffer[0] =  header;
+//     newBuffer = malloc(strlen(header)+1+length);
+//     strcpy(newBuffer, header);
+//     strcat(newBuffer, buffer);
+
+//     if(l2_write(newBuffer, length+1) == -1){
+//         return -1;
+//     }
+
+//     return length;
+// }
+
+int l3_write(char* buffer, int length){
+    unsigned short checksum = htons(checksum(buffer, length));
+    char header[2];
+
+    memcpy(&header, &checksum, sizeof(header));
+
+    char* newBuffer;
+
+    newBuffer = malloc(strlen(header)+1+length);
+    strcpy(newBuffer, header);
     strcat(newBuffer, buffer);
 
     if(l2_write(newBuffer, length+1) == -1){
