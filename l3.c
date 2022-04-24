b757460125da35e977d2979ccc9097328c0f8b12
brnngn <60111880+brnngn@users.noreply.github.com>
2022-04-24 16:43:53 -0400

    Update l3.c

--- l3.c
+++ l3.c
@@ -4,13 +4,25 @@
 extern int l2_read(char* buffer, int maxlength);
 extern int l2_write(char* buffer, int length);
 
-unsigned char checksum(char* buffer, int length){
-    unsigned char sum;
-    sum = *buffer;
-    for (int i = 1; i<length; i++){
-        sum = (char) (sum + *(buffer+i));
-    }
-    return sum;
+// unsigned char checksum(char* buffer, int length){
+//     unsigned char sum;
+//     sum = *buffer;
+//     for (int i = 1; i<length; i++){
+//         sum = (char) (sum + *(buffer+i));
+//     }
+//     return sum;
+// }
+
+unsigned short checksum(char* buffer, int length){
+   int i;
+   unsigned short *p;
+   unsigned int sum = 0;
+   p = (unsigned short *) buffer;
+   for(i=0; i < length/2; i++){
+      sum = sum + htons(p[i]);
+      if (sum&0x10000) sum = (sum&0xFFFF)+1;
+   }
+   return (unsigned short)0xFFFF-sum;
 }
 
 
