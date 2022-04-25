204021b3b56b8639d2dc00a26ea96e5a898fbb67
brnngn <60111880+brnngn@users.noreply.github.com>
2022-04-24 21:37:12 -0400

    Update l3.c

--- l3.c
+++ l3.c
@@ -1,24 +1,19 @@
 // l3.c
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <arpa/inet.h>
 
 // Interface to Layer 2
 extern int l2_read(char* buffer, int maxlength);
 extern int l2_write(char* buffer, int length);
 
-// unsigned char checksum(char* buffer, int length){
-//     unsigned char sum;
-//     sum = *buffer;
-//     for (int i = 1; i<length; i++){
-//         sum = (char) (sum + *(buffer+i));
-//     }
-//     return sum;
-// }
-
 unsigned short checksum(char* buffer, int length){
-   int i;
    unsigned short *p;
    unsigned int sum = 0;
    p = (unsigned short *) buffer;
-   for(i=0; i < length/2; i++){
+   for(int i=0; i < length/2; i++){
       sum = sum + htons(p[i]);
       if (sum&0x10000) sum = (sum&0xFFFF)+1;
    }
@@ -30,11 +25,12 @@ int l3_read(char* buffer, int maxlength)
 {
     char *messageWithHeader;
     int messageLength = l2_read(messageWithHeader, maxlength);
-    if ( (messageLength == -1)|(messageLength > maxlength) ){
+   
+    if ( (messageLength == -1) || (messageLength > maxlength) ){
         return -1;
     } 
 
-    char checksumOfMessage = checksum(messageWithHeader, messageLength) &= 0x7F ;
+    unsigned short checksumOfMessage = checksum(messageWithHeader, messageLength);
 
     if (checksumOfMessage == 0){
         for (int i = 1; i < messageLength; i++){
@@ -52,19 +48,18 @@ int l3_read(char* buffer, int maxlength)
 int l3_write(char* buffer, int length)
 {
     
-    unsigned char checksumOutput = checksum(buffer, length);
-    checksumOutput = (char) ((-checksumOutput) & 0x7F);
+    unsigned short checksumOutput = checksum(buffer, length);
+    char header[2];
+    uint16_t messageLengthPreConvert = htons((uint16_t)checksumOutput);
+    memcpy(header, &messageLengthPreConvert, sizeof(uint16_t));
+    int newLength = length + sizeof(header);
+    char newBuffer[newLength];
+    
+    memcpy(newBuffer, header, sizeof(header));
+    memcpy(newBuffer+sizeof(header), buffer, length);
+    
 
-    char header[1];
-    header[0] = checksumOutput;
-
-    char* newBuffer;
-
-    newBuffer = malloc(strlen(header)+1+length);
-    strcpy(newBuffer, header);
-    strcat(newBuffer, buffer);
-
-    if(l2_write(newBuffer, length+1) == -1){
+    if(l2_write(newBuffer, newLength) == -1){
         return -1;
     }
 
